d74c1aac423d229013234046c93da9c2f19fce2d
gbandala <bandala@outlook.com>
2025-06-10 14:39:48 -0400

    delete google sign in

--- src/pages/auth/components/sign-in-form.tsx
+++ src/pages/auth/components/sign-in-form.tsx
@@ -152,13 +152,13 @@ export function SignInForm({
 
             {/* {OR} */}
 
-            <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+            {/* <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
               <span className="relative z-10 bg-background px-2 text-muted-foreground">
                 Or
               </span>
-            </div>
+            </div> */}
 
-            <div className="grid gap-1 sm:grid-cols-1 -mt-3">
+            {/* <div className="grid gap-1 sm:grid-cols-1 -mt-3">
               <Button variant="outline" className="w-full">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path
@@ -168,7 +168,7 @@ export function SignInForm({
                 </svg>
                 Continue with Google
               </Button>
-            </div>
+            </div> */}
 
             <div className="text-center text-sm">
               Don&apos;t have an account?{" "}
